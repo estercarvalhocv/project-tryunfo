d9ea843d9ed8395c64cfeab04c296a6cdbc24b3c
Ester Borges de Carvalho <esterborgesdecarvalho@gmail.com>
2022-05-18 20:37:23 -0300

    agora vai

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 // import PropTypes from 'prop-types';
 
-import React from "react";
+import React from 'react';
 
 class Form extends React.Component {
   render() {
@@ -20,75 +20,80 @@ class Form extends React.Component {
     } = this.props */
     return (
       <form>
-        <label htmlFor='name-input'>
-          Nome
-          <input
-            data-testid="name-input"
-            type="text"
-          />
-        </label>
-        <label htmlFor='description-input'>
-          Descrição
-          <input
-            data-testid="description-input"
-            type="textarea"
-          />
-        </label>
-        <label htmlFor='attr1-input'>
-          Atributo 1
-          <input
-            data-testid="attr1-input"
-            type="number"
-          />
-        </label>
-        <label>
-          Atributo 2
-          < input
-            data-testid="attr2-input"
-            type="number"
-          />
-        </label>
-        <label>
-          Atributo 3
-          <input
-            data-testid="attr3-input"
-            type="number"
-          />
-        </label>
-        <label>
-          <input
-            data-testid="image-input"
-            type="number"
-          />
-        </label>
-        <label>
-          < select
-            data-testid="rare-input" >
-            <option>normal</option>
-            <option>raro</option>
-            <option>muito raro</option>
-          </select>
-        </label>
-        <label htmlFor="trunfo-input">
-          <input
-            data-testid="trunfo-input"
-            type="checkbox"
-            id="trunfo-input"
-          />
-          Super Trybe Trunfo
-        </label>
-        <button
-          data-testid="save-button"
-          type="button"
-          id="save-button"
-        >
-          Salvar
-        </button>
+        <control>
+          <label htmlFor="name-input">
+            Nome
+            <input
+              data-testid="name-input"
+              type="text"
+            />
+          </label>
+          <label htmlFor="description-input">
+            Descrição
+            <input
+              data-testid="description-input"
+              type="textarea"
+            />
+          </label>
+          <label htmlFor="attr1-input">
+            Atributo 1
+            <input
+              data-testid="attr1-input"
+              type="number"
+            />
+          </label>
+          <label htmlFor="attr2">
+            Atributo 2
+            <input
+              data-testid="attr2-input"
+              type="number"
+            />
+          </label>
+          <label htmlFor="attr3">
+            Atributo 3
+            <input
+              data-testid="attr3-input"
+              type="number"
+            />
+          </label>
+          <label htmlFor="image-input">
+            Imagem
+            <input
+              data-testid="image-input"
+              type="number"
+            />
+          </label>
+          <label htmlFor="rare-input">
+            Raridade da Carta
+            <select
+              data-testid="rare-input"
+            >
+              <option>normal</option>
+              <option>raro</option>
+              <option>muito raro</option>
+            </select>
+          </label>
+          <label htmlFor="trunfo-input">
+            Super Trybe Trunfo
+            <input
+              data-testid="trunfo-input"
+              type="checkbox"
+              id="trunfo-input"
+            />
+          </label>
+          <button
+            data-testid="save-button"
+            type="button"
+            id="save-button"
+          >
+            Salvar
+          </button>
+        </control>
       </form>
     );
   }
-};
-/* form.propTypes = 
+}
+/* form.propTypes =
   cardName,
   cardDescription,
   cardAttr1,
@@ -96,12 +101,11 @@ class Form extends React.Component {
   cardAttr3,
   cardImage,
   cardRare,
-  cardTrunfo */
-  // hasTrunfo,
-  // isSaveButtonDisabled,
-  // onInputChange,
-  // onSaveButtonClick
-
+  cardTrunfo,
+  hasTrunfo,
+  isSaveButtonDisabled,
+  onInputChange,
+  onSaveButtonClick */
 
 export default Form;
-// corriginto espaçamento
+/* corriginto espaçamento */
