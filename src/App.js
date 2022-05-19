7c36ebe91a2af055828f75bd91c6c94d2bfe40d5
Ester Borges de Carvalho <esterborgesdecarvalho@gmail.com>
2022-05-19 10:28:13 -0300

    requiriment 6 OK

--- src/App.js
+++ src/App.js
@@ -43,11 +43,27 @@ class App extends React.Component {
     this.setState({ [id]: value }, this.btnValidation);
   }
 
+  onSaveButtonClick = () => {
+    this.setState({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Adiciona nova carta</h1>
-        <Form { ...this.state } onInputChange={ this.handleChange } />
+        <Form
+          { ...this.state }
+          onInputChange={ this.handleChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
         <Card { ...this.state } />
       </div>
     );
